26183fb9b0229aaf2977107249c0505073f5d0b8
ramon_ramos_ext <ramon_ramos_ext@carrefour.com>
2024-02-19 10:21:21 -0300

    feat(brand): troca m3 por cadastra em envs

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,7 +1,7 @@
 export const authConfig = {
   options: {
     jwt: {
-      secret: process.env.JWT_SECRET ?? 'm3ctf',
+      secret: process.env.JWT_SECRET ?? 'cadastractf',
       expiration: process.env.JWT_EXPIRATION ?? '5d',
     },
   },
